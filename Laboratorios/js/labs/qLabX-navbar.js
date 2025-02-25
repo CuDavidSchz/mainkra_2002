3f7fce2004c96929db379e7425ba89caf05d25ea
davidcuschz <davidcuschz@gmail.com>
2025-02-24 21:11:27 -0600

    Sandbox for Node.js Labs Ready

--- Laboratorios/js/labs/qLabX-navbar.js
+++ Laboratorios/js/labs/qLabX-navbar.js
@@ -1,15 +1,3 @@
-
-/* 
-Función del menu laboratorios SIN BULMA
-*/
-
-document.getElementById("laboratorios").addEventListener("change", function() {
-    const url = this.value;
-    if (url) {
-        window.location.href = url; 
-    }
-});
-
 /*
     Función del menú con BULMA  Mobile
 */
